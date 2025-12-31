6260142360bc68506dfa3a62f818f48f4ae538db
motiefard <mozhdemotieifard@gmail.com>
2025-12-31 15:07:20 +0330

    add raw-asset and define it in defenition.py

--- news_pipeline/news_pipeline/definitions.py
+++ news_pipeline/news_pipeline/definitions.py
@@ -1,9 +1,14 @@
 from dagster import Definitions, load_assets_from_modules
 
-from news_pipeline import assets  # noqa: TID252
+from news_pipeline import assets
+from .assets import raw_news_articles
+from .resources import news_api_resource
 
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
-    assets=all_assets,
+    assets=[raw_news_articles],
+    resources={
+        "news_api": news_api_resource,
+    },
 )
