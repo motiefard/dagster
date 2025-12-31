9a6a3950260b05511cda87dae0e8383c03b425b2
motiefard <mozhdemotieifard@gmail.com>
2025-12-31 16:05:25 +0330

    clean & normalize: add asset into definition

--- news_pipeline/news_pipeline/definitions.py
+++ news_pipeline/news_pipeline/definitions.py
@@ -1,13 +1,13 @@
 from dagster import Definitions, load_assets_from_modules
 
 from news_pipeline import assets
-from .assets import raw_news_articles
+from .assets import raw_news_articles, cleaned_news_articles
 from .resources import news_api_resource
 
 all_assets = load_assets_from_modules([assets])
 
 defs = Definitions(
-    assets=[raw_news_articles],
+    assets=[raw_news_articles, cleaned_news_articles],
     resources={
         "news_api": news_api_resource,
     },
